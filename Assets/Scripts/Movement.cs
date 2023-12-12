3b09eb7a1faf4b91f36a90f2a0ba99364638ce10
Fabián E. García <fabianegl79@gmail.com>
2023-12-12 10:50:49 -0300

    Physics2D y rotation a los asteroides

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,8 +7,12 @@ public class Movement : MonoBehaviour
     public Vector3 direction;
     public float speed;
 
+    //public Vector3 rotation;
+    //public float speedRotation;
+
     private void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime);
+        //transform.rotation 
     }
 }
---
+++ Assets/Scripts/OnCollisionDo.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnCollisionDo : MonoBehaviour
+{
+    [SerializeField] private UnityEvent action;
+
+    private GameObject collisionee;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collisionee = collision.gameObject;
+        action.Invoke();
+    }
+
+    public void DestroyCollisionee()
+    {
+        if (collisionee != null)
+        {
+            Destroy(collisionee);
+        }
+    }
+
+}
